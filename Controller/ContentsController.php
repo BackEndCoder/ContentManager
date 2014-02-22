312be12e5b43471620bf9f07e2afe9c9d4e01d71
BackEndCoder <steve.coverdale@gmail.com>
2014-02-22 09:49:45 +0000

    Updated actions

--- Controller/ContentsController.php
+++ Controller/ContentsController.php
@@ -1,72 +1,84 @@
 <?php
 App::uses('ContentManagerAppController', 'ContentManager.Controller');
 class ContentsController extends ContentManagerAppController {
-    public $name = 'Contents';
-    public $helpers = array('Html');
+
+	public $components = array('Paginator', 'Session');
 
     public function beforeFilter() {
         parent::beforeFilter();
         $user = $this->Auth->user();
         $this->set('current_user', $user);
-        $this->Auth->allow('display');
         $this->Auth->allow('show');
+    }
+
+	public function view($id = null) {
+		if (!$this->Content->exists($id)) {
+			throw new NotFoundException(__('Invalid content'));
+		}
+		$options = array('conditions' => array('Content.' . $this->Content->primaryKey => $id));
+		$this->set('content', $this->Content->find('first', $options));
+	}
+
+	public function admin_index() {
+		$this->Content->recursive = 0;
+		$this->set('contents', $this->Paginator->paginate());
+	}
+
+	public function admin_view($id = null) {
+		if (!$this->Content->exists($id)) {
+			throw new NotFoundException(__('Invalid content'));
+		}
+		$options = array('conditions' => array('Content.' . $this->Content->primaryKey => $id));
+		$this->set('content', $this->Content->find('first', $options));
         $this->layout = "admin";
-    }
+	}
 
-    public function add() {
-        if ($this->request->is('post')) {
-            if ($this->Content->save($this->data)) {
-                $this->redirect(array('action' => 'index'));
-                $this->Session->setFlash("The content has been saved");
-            } else {
-                $this->Session->setFlash('The content could not be saved. Please, try again.');
-            }
-        }
-    }
+	public function admin_add() {
+		if ($this->request->is('post')) {
+			$this->Content->create();
+			if ($this->Content->save($this->request->data)) {
+				$this->Session->setFlash(__('The content has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The content could not be saved. Please, try again.'));
+			}
+		}
+        $this->layout = "admin";
+	}
 
-    public function edit($id = null) {
-        $this->Content->id = $id;
-        if ($this->request->is('post') || $this->request->is('put')) {
-            if ($this->Content->save($this->request->data)) {
-                $this->Session->setFlash('The product has been saved');
-                $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash('The product could not be saved. Please, try again.');
-            }
-        } else {
-            $this->request->data = $this->Content->read();
-        }
-    }
+	public function admin_edit($id = null) {
+		if (!$this->Content->exists($id)) {
+			throw new NotFoundException(__('Invalid content'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Content->save($this->request->data)) {
+				$this->Session->setFlash(__('The content has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The content could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Content.' . $this->Content->primaryKey => $id));
+			$this->request->data = $this->Content->find('first', $options);
+		}
+        $this->layout = "admin";
+	}
 
-    public function view($id = null) {
-        $this->Content->id = $id;
+	public function admin_delete($id = null) {
+		$this->Content->id = $id;
+		if (!$this->Content->exists()) {
+			throw new NotFoundException(__('Invalid content'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Content->delete()) {
+			$this->Session->setFlash(__('The content has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The content could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+        $this->layout = "admin";
+	}
 
-        if (!$this->Content->exists()) {
-            throw new NotFoundException('Invalid Content');
-        }
-
-        if (!$id) {
-            $this->Session->setFlash('Invalid Content');
-            $this->redirect(array('action' => 'index'));
-        }
-        $this->set('content', $this->Content->read());
-    }
-
-    public function show($id = null) {
-        $this->Content->id = $id;
-
-        if (!$this->Content->exists()) {
-            throw new NotFoundException('Invalid Content');
-        }
-
-        if (!$id) {
-            $this->Session->setFlash('Invalid Content');
-            $this->redirect(array('action' => 'index'));
-        }
-        $this->set('content', $this->Content->read());
-        $this->layout = "default";
-    }
-	
     public function getContent($id) {
         return $this->Content->getContent($id);
     }
--- Model/Content.php
+++ Model/Content.php
@@ -1,7 +1,11 @@
 <?php
 App::uses('ContentManagerAppModel', 'ContentManager.Model');
 class Content extends ContentManagerAppModel {
+
 	public $useTable = 'pages';
+
+	public $displayField = 'title';
+
     public function getContent($id) {
         return $this->find('first', array('conditions' => array('id' => $id)));
     }
